00d8219d3cc4a005dce5e93117d64700dd53fe97
Jordi Franco <jordi.franco@scmspain.com>
2020-11-07 21:29:29 +0100

    added logic for proxyscan.io api

--- proxy-scrapper.py
+++ proxy-scrapper.py
@@ -5,7 +5,6 @@ import json
 filename = 'proxylist.jdproxies'
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36' \
              '(KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
-proxy_site_url = 'https://socks-proxy.net/'
 
 
 def create_proxy_record(address, port, type, enabled):
@@ -35,20 +34,44 @@ def create_json_structure(proxies):
     return proxylist_json_structure
 
 
-res = requests.get(proxy_site_url, headers={'User-Agent': user_agent})
-soup = BeautifulSoup(res.text, "lxml")
-proxy_list = [create_proxy_record(type="NONE", address=None, port=80, enabled=True)]
+def get_proxies_from_free_proxy_net():
+    proxy_site_url = 'https://socks-proxy.net/'
+    res = requests.get(proxy_site_url, headers={'User-Agent': user_agent})
+    soup = BeautifulSoup(res.text, "lxml")
+    proxy_list = list()
 
-for items in soup.select("#proxylisttable tbody tr"):
-    proxy_definition = []
-    for item in items.select("td")[:8]:
-        proxy_definition.append(item.text)
-    proxy_list.append(
-        create_proxy_record(
-            type=proxy_definition[4].upper(), address=proxy_definition[0], port=int(proxy_definition[1]), enabled=True
+    for items in soup.select("#proxylisttable tbody tr"):
+        proxy_definition = []
+        for item in items.select("td")[:8]:
+            proxy_definition.append(item.text)
+        proxy_list.append(
+            create_proxy_record(
+                type=proxy_definition[4].upper(), address=proxy_definition[0], port=int(proxy_definition[1]), enabled=True
+            )
         )
-    )
+    return proxy_list
 
+def get_proxies_from_proxyscan_io():
+    proxy_site_url = 'https://www.proxyscan.io/api/proxy?last_check=9800&ping=500&limit=20&type=socks4,socks5,https'
+    res = requests.get(proxy_site_url, headers={'User-Agent': user_agent})
+    proxies = json.loads(res.text)
+    for item in proxies:
+        if len(item["Type"]) > 1:
+            if "SOCKS5" in item["Type"]:
+                item["Type"] = "SOCKS5"
+            else:
+                item["Type"] = "HTTPS"
+        else:
+            proxy_list.append(
+                create_proxy_record(
+                    type=item["Type"], address=item["Ip"], port=item["Port"], enabled=True
+                )
+            )
+    return proxy_list
+
+proxy_list = list([create_proxy_record(type="NONE", address=None, port=80, enabled=True)])
+proxy_list.append(get_proxies_from_proxyscan_io())
+del proxy_list[-1]
 json_output = create_json_structure(proxy_list)
 with open(filename, 'w') as f:
     json.dump(json_output, f, indent=2)
